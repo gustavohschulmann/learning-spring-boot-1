cf38899a085fc03c5d6f196ebee65a460d0dafb1
Gustahs <44182375+gustavohschulmann@users.noreply.github.com>
2020-09-25 13:55:33 -0300

    Adding Initial Files

---
+++ learning-spring-gustavo/src/main/java/com/gustavoschulmann/lil/learningspring/data/repository/ReservationRepository.java
@@ -0,0 +1,12 @@
+package com.gustavoschulmann.lil.learningspring.data.repository;
+
+import java.sql.Date;
+
+import com.gustavoschulmann.lil.learningspring.data.entity.Reservation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+    Iterable<Reservation> findReservationByReservationDate(Date date);
+}
